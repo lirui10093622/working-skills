86bb7181c43ca10cd800d8f854a1ec583510e42e
rui.li04 <rui.li04@mljr.com>
2018-01-06 16:41:20 +0800

    新增bat脚本

---
+++ bat/build-all-project.bat
@@ -0,0 +1,18 @@
+echo off
+rem 作者: 李锐
+rem 邮箱: lirui10093622@163.com
+rem 版本: 1.0.0.20180106160900
+rem 说明: 构建(该脚本所在)目录下的所有maven项目
+
+for /d %%a in ( ./* ) do (
+  if "%%a" neq ".idea" (
+    echo 构建项目: %%a
+    cd %%a
+    if exist "pom.xml" (
+      call mvn clean install -DskipTests
+    )
+	cd ..
+	echo off
+  )
+)
+pause
--- bat/pull-all-repository.bat
+++ bat/pull-all-repository.bat
@@ -1,17 +1,17 @@
 echo off
 rem 作者: 李锐
-rem 邮箱: lirui10093622
-rem 版本: 1.0.0.20180106133400
+rem 邮箱: lirui10093622@163.com
+rem 版本: 1.0.0.20180106162400
 rem 说明: 更新(该脚本所在)目录下的所有git项目
 
 for /d %%a in ( ./* ) do (
-if "%%a" neq ".idea" (
- echo 更新项目: %%a
- cd %%a
- git fetch --all
- git checkout master
- git merge origin/master
- cd ..
- )
+  if "%%a" neq ".idea" (
+    echo 更新项目: %%a
+    cd %%a
+    git fetch --all
+    git checkout master
+    git merge origin/master
+    cd ..
+  )
 )
 pause
